626dcc1bc2420b4decc9f8ffff3faaaa2091eac0
Antonio Sitoe <antoniositoehl@gmail.com>
2024-02-25 15:25:34 +0200

    ds

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -57,7 +57,7 @@ export default function TabLayout() {
           }}
         />
         <Tabs.Screen
-          name="Exercicies"
+          name="Exercises"
           options={{
             tabBarButton: () => null,
           }}
